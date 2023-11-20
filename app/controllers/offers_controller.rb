0c4a959adebfee81cb842b6f5f5b1a65a4d2a4d0
lilian chabaus <lilian.chbd@gmail.com>
2023-11-20 15:04:47 +0100

    controller-offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,13 +1,43 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: %i[show edit update destroy]
   def index
+    @offers = Offer.all
   end
 
   def show
   end
 
   def new
+    @offer = Offer.new
   end
 
   def edit
   end
+
+  def create
+    offer = Offer.new(params_offer)
+    offer.save
+    redirect_to offers_path
+  end
+
+  def update
+    @offer.update(params_offer)
+    redirect_to offers_path(@offer)
+  end
+
+  def destroy
+    @offer.destroy
+    redirect_to offers_path
+  end
+
+private
+
+def params_offer
+  params.require(:offer).permit(:title, :price, :content, :category)
+end
+
+def set_offer
+  @offer = Offer.find(params[:id])
+end
+
 end
