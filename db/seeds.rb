cd7e591c01461eb5fb58dcd651326609cc91da01
thomas laizé <thomas33130@hotmail.fr>
2023-11-22 14:17:12 +0100

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -10,49 +11,17 @@
 
 
 puts "start"
-
+Offer.destroy_all
+User.destroy_all
 puts "create first user"
 user = User.create(email: "contact@artloc.com", password: "123456")
 puts "user created"
 
-
-require "open-uri"
-
-file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Leonardo_da_vinci%2C_la_gioconda%2C_1503-06_circa.jpg/220px-Leonardo_da_vinci%2C_la_gioconda%2C_1503-06_circa.jpg")
-article = Article.new(title: "La Joconde", content: "La Joconde (en italien: La Gioconda ), ou Portrait de Mona Lisa, est un tableau de l'artiste Léonard de Vinci, réalisé entre 1503 et 1506 ", price: 10000000, category:"peinture")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
-file = URI.open("https://d1ee3oaj5b5ueh.cloudfront.net/thumbs/787xAUTO_original_article_2023_08_f204e49d-84eb-45e2-bbe8-164c4a6d684e.jpg")
-article = Article.new(title: "Salvator Mundi", content: "", price: 4500000, category:"peinture")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
-file = URI.open("https://www.francetvinfo.fr/pictures/g7mlvm_QT74yVwzUizya-e3Lve0/1200x1200/2019/04/12/banksy_2.jpg")
-article = Article.new(title: "Girl with balloon", content: "A great console", price: 200000, category:"peinture")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
-file = URI.open("https://upload.wikimedia.org/wikipedia/commons/c/cc/Madonna_Lily_Egg.jpg")
-article = Article.new(title: "Oeuf de Fabergé", content: "A great console", price: 200000, category:"peinture")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
-file = URI.open("https://arthisto.fr/wp-content/uploads/2016/11/buste-de-nefertiti.jpg")
-article = Article.new(title: "Buste de la reine Nefertiti", content: "A great console", price: 50000, category:"sculpture")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
-file = URI.open("https://cdn-s-www.leprogres.fr/images/E3199B5E-D4D3-4E7C-8C02-5A2303E97A91/NW_raw/jeff-koons-est-connu-entre-autres-pour-ses-sculptures-semblant-faites-avec-des-ballons-comme-ici-ce-balloon-dog-bleu-ici-d-une-taille-imposante-photo-sipa-rex-shutterstock-1676795360.jpg")
-article = Article.new(title: "Jeff Koons", content: "A great console", price: 5000, category:"peinture")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
-file = URI.open("https://balcondart.com/wp-content/uploads/2019/12/191205-art-basel-banana.jpg")
-article = Article.new(title: "Banane", content: "A great console", price: 1500, category:"objet")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-article.save
-
+file = [
+  URI.open("https://www.wpbeginner.com/wp-content/uploads/2020/03/ultimate-small-business-resource-coronavirus.png"),
+  URI.open("https://media.gettyimages.com/id/1365857239/fr/vectoriel/petites-ic%C3%B4nes-de-ligne-dinterface-utilisateur-contour-modifiable.jpg?s=612x612&w=gi&k=20&c=kIQh1JdFMFwkLXX_twHoS7ifJIuMzhBcfsw-1iMGKKk="),
+  URI.open("https://www.shutterstock.com/image-vector/compact-size-small-scale-fit-260nw-1590167689.jpg"),
+]
 
 offers = [
 
@@ -61,11 +30,24 @@ offers = [
   {title: "Cape d'invisibilité", content: "Devenez 100% transparent!", price: 99.99, category: "Sorcier" }
 
 ]
+
+results = []
 count = 1
 offers.each do |offer|
   offer[:user] = user
-  Offer.create!(offer)
+  offer_test = Offer.new(offer)
+  offer_test.save!
+  results << offer_test
   puts count
+  count += 1
+end
+
+puts "pictures"
+
+count = 0
+results.each do |result|
+  puts "start picture"
+  result.photo.attach(io: file[count], filename: "nes.png", content_type: "image/png")
   count += 1
 end
 
