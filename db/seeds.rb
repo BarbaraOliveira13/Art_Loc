0c6116bbcd016a195efb328852ed3a11615daf31
barbara oliveira <barbarafictiovore@gmail.com>
2023-11-24 15:49:35 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,14 @@ require "open-uri"
 
 
 puts "start"
+Reservation.destroy_all
 Offer.destroy_all
 User.destroy_all
+
 puts "create first user"
-user = User.create(email: "contact@artloc.com", password: "123456")
+user = User.create(email: "contact@artloc.com", password: "123456", password_confirmation: "123456")
+user_l = User.create(email: "lilianbg@artloc.com", password: "123456", password_confirmation: "123456")
+user_b = User.create(email: "barbara@artloc.com", password: "123456", password_confirmation: "123456")
 puts "user created"
 
 file = [
@@ -25,38 +29,32 @@ file = [
   URI.open("https://www.centrepompidou.fr/media/picture/ba/b4/bab45a073cd09f2b8adbdd6888c26ee1/thumb_large.jpg"),
   URI.open("https://balcondart.com/wp-content/uploads/2019/12/191205-art-basel-banana.jpg"),
   URI.open("https://images.rtl.fr/~c/1200v800/rtl/www/1101227-une-grande-exposition-a-la-fondation-vuitton.jpg"),
+  URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Rodin_-_Le_Baiser_06.jpg/1024px-Rodin_-_Le_Baiser_06.jpg"),
+  URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"),
+
+  URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/The_Kiss_-_Gustav_Klimt_-_Google_Cultural_Institute.jpg/1280px-The_Kiss_-_Gustav_Klimt_-_Google_Cultural_Institute.jpg"),
+
 ]
 
+  Offer.create!(title: "La Joconde", content: "La Joconde, ou Portrait de Mona Lisa, est un tableau de l'artiste Léonard de Vinci, réalisé entre 1503 et 1506.", price: 5_000_000, category: "Peinture", user_id: user.id)
+  Offer.create!(title: "Oeuf de Fabergé", content: "Les œufs de Fabergé sont des objets précieux créés par le joillier Karl Fabergé né en Russie à Saint Petersbourg. ", price: 15_000, category: "Art", user_id: user_l.id)
+  Offer.create!(title: "Salvator Mundi", content: "Le Salvator Mundi est une peinture à l'huile sur bois de noyer, sur le thème du Christ rédempteur, attribuée par plusieurs experts à Léonard de Vinci", price: 2_000_000, category: "Peinture", user_id: user.id)
+  Offer.create!(title: "La Petite Fille au ballon", content: "est une série d'œuvres d'art urbain par Banksy, une peinture au pochoir apparue pour la première fois en 2002 à Londres sur le pont de Waterloo à South Bank. ", content: "7 Boundary St, London E2 7JE", price: 5500, category: "Peinture", user_id: user.id)
+  Offer.create!(title: "Balloon Dog", content: "Balloon Dogs est une sculpture monumentale de l'artiste américain Jeff Koons représentant un chien sculpté à partir d'un ballon.", price: 6_900, category: "french", user_id: user.id)
+  Offer.create!(title: "Banane", content: "Nommée Comedian, l'œuvre d'art, réalisée par Maurizio Cattelan, a été acquise pour 120 000 dollars par un collectionneur de nationalité française.", price: 3900.90, category: "Sorcier", user_id: user.id)
+  Offer.create!(title: "Le Cri", content: "Le Cri est une œuvre expressionniste de l'artiste norvégien Edvard Munch dont il existe cinq versions réalisées entre 1893 et 1917. Symbolisant l'homme moderne emporté par une crise d'angoisse existentielle.", price: 6900.99, category: "Sorcier", user_id: user.id)
+  Offer.create!(title: "Le baiser", content: "Le Baiser est une sculpture représentant un couple enlacé, créée vers 1881 par Auguste Rodin, initialement comme motif pour La Porte de l'Enfer. ", price: 12_000_000, category: "Sculpture", user_id: user_l.id)
+  Offer.create!(title: "La naissance de Vénus", content: "La Naissance de Vénus est un tableau de Sandro Botticelli, peint vers 1484-1485 et conservé à la galerie des Offices. ", price: 15_000, category: "Art", user_id: user_l.id)
 
+  Offer.create!(title: "Le baiser", content: "Gustav Klimt (1909)", price: 12_000_000, category: "Peinture", user_id: user_b.id)
 
-
-offers = [
-  {title: "La Joconde", content: "a", price: 5_000_000, category: "Peinture" },
-  {title: "Oeuf de Fabergé", content: "Bois de sureau et plume d'aigle", price: 15_000, category: "Art" },
-  {title: "Salvator Mundi", content: "Devenez 100% transparent!", price: 2_000_000, category: "Peinture" },
-  {title: "La petite fille au balon ", content: "7 Boundary St, London E2 7JE", price: 5500, category: "Peinture" },
-  {title: "Ballon Dog", content: "7 Boundary St, London E2 7JE", price: 6_900, category: "french" },
-  {title: "Banane", content: "Bois de sureau et plume d'aigle", price: 3900.90, category: "Sorcier" },
-  {title: "Le Cri", content: "Devenez 100% transparent!", price: 6900.99, category: "Sorcier" },
-]
-
-results = []
-count = 1
-offers.each do |offer|
-  offer[:user] = user
-  offer_test = Offer.new(offer)
-  offer_test.save!
-  results << offer_test
-  puts count
-  count += 1
-end
 
 puts "pictures"
 
 count = 0
-results.each do |result|
+Offer.all.each do |offer|
   puts "start picture"
-  result.photo.attach(io: file[count], filename: "nes.png", content_type: "image/png")
+  offer.photo.attach(io: file[count], filename: "nes.png", content_type: "image/png")
   count += 1
 end
 
