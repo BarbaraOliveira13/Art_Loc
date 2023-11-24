650bbd59cc4e6bb599985bc17dfed5e709abae41
thomas laizé <thomas33130@hotmail.fr>
2023-11-24 12:39:44 +0100

    save

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
+pin "aos", to: "https://ga.jspm.io/npm:aos@2.3.4/dist/aos.js"
